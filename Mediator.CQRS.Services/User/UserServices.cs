43ecf1e1fc69ea3880ab0b6f051cc0fb60b39372
Oussama-souissi024 <oussama.souissi024@gmail.com>
2024-10-09 13:07:41 +0100

    first commit

---
+++ Mediator.CQRS.Services/User/UserServices.cs
@@ -0,0 +1,45 @@
+﻿using Mediator.CQRS.Core.Interfaces;
+using Mediator.CQRS.Shared.Models;
+
+namespace Mediator.CQRS.Services.User
+{
+    public class UserServices : IUserServices
+    {
+        private static List<UserModel> _users = new List<UserModel>() 
+        {
+            new UserModel()
+            {
+                Id = 1,
+                FirstName = "Oussama",
+                LastName = "Souissi"
+            },
+            new UserModel()
+            {
+                Id = 2,
+                FirstName = "Hamdi",
+                LastName = "Jouini"
+            },
+            new UserModel()
+            {
+                Id = 3,
+                FirstName = "Khalil",
+                LastName = "Arfaoui"
+            },
+        };
+        public UserModel AddUser(UserModel user)
+        {
+            _users.Add(user);
+            return user;
+        }
+
+        public UserModel GetUserByIndex(int index)
+        {
+            return _users[index];
+        }
+
+        public List<UserModel> GetUsers()
+        {
+            return _users;
+        }
+    }
+}
